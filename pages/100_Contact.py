5bdf992aed1485c0fb947ee8316c919539378c78
Mark Thompson <markthompsonr@gmail.com>
2023-06-24 12:19:38 +0100

    Add contact page

---
+++ pages/100_Contact.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Contact")
+st.subheader("_Problems? Feedback?_")
+st.write(
+    """
+    If you want to get in touch for any reason - encountering bugs or problems; feedback for improvements; feedback for 
+    appreciation - you can use the following contact options:
+    
+    - Email: getgoalside.newsletter+tutorials@gmail.com
+    - Twitter: [@get_goalside](https://twitter.com/get_goalside) (DMs are open)
+    """
+)
